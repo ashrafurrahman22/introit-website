44651134b5fa345fba6b00f67f842610c71443ee
ashrafur22 <ashrafurrahman22@gmail.com>
2022-07-23 21:45:35 +0600

    footer-updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Help from './Pages/Home/Help';
 import Home from './Pages/Home/Home';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
     <div>
       <Home></Home>
+      <Footer></Footer>
     </div>
   );
 }
