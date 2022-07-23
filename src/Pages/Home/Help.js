5f7e3052fab08e6c912ac76825af701810cb744e
ashrafur22 <ashrafurrahman22@gmail.com>
2022-07-23 12:11:56 +0600

    connecting-with-github

---
+++ src/Pages/Home/Help.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Styles/Help.css'
+
+const Help = () => {
+    return (
+        <div className='w-2/5 my-10 mx-auto font-aleo'>
+           <div>
+           <h2 id='headings' className='my-2 font-bold'>How can we help you?</h2>
+            <p style={{fontFamily:"Aleo", fontSize:"18px"}} className='text-center font-medium text-md'>If you’re looking for a web development company that can get the job done, then you need to look no further than [our company]. We have the experience, the skills, and the track record to prove that we are the best in the business. Contact us today to learn more about what we can do for you</p>
+           </div>
+            
+        </div>
+    );
+};
+
+export default Help;
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Help from './Help';
+
+const Home = () => {
+    return (
+        <div>
+            <Help></Help>
+        </div>
+    );
+};
+
+export default Home;
