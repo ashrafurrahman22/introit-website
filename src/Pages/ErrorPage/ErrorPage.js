cd342f7cd9d5ba74341100dc0eff43915caae13b
ashrafur22 <ashrafurrahman22@gmail.com>
2022-07-27 22:05:35 +0600

    erro-page-updated

--- src/Pages/ErrorPage/ErrorPage.js
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div>
-            <img src="https://media4.giphy.com/media/UoeaPqYrimha6rdTFV/giphy.gif" alt="" />
+        <div className='mx-auto flex flex-col gap-5 justify-center w-2/4'>
+            <img className='mx-auto w-full' src="https://digitalagencynetwork.com/wp-content/uploads/2019/08/best-digital-agency-404-pages-for-your-inspiration.jpg" alt="" />
+            <Link to='/' className="btn btn-info font-medium my-5 hover:bg-blue-500 justify-center">Back to HomePage?</Link>
         </div>
     );
 };
